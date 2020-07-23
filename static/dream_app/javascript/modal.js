014b013ecd8fe561f7e31bb58d0058edf36f4808
brysonmckenzie <brysonmckenzie83@gmail.com>
2020-07-23 07:22:00 -0700

    update

--- static/dream_app/javascript/modal.js
+++ static/dream_app/javascript/modal.js
@@ -1,17 +1,35 @@
 
 
-var span =  document.getElementsByClassName("ex")[0];
+// var span =  document.getElementsByClassName("ex")[0];
 
 
-console.log('working')
+// console.log('working')
 
-function clearModal() {
-    modal.style.display = "none";
-}
+// function clearModal() {
+//     modal.style.display = "none";
+// }
 
-window.onclick = function(event) {
-    if (event.target === span) {
-        clearModal();
-    }
-}
+// window.onclick = function(event) {
+//     if (event.target === span) {
+//         clearModal();
+//     }
+// }
 
+// var exitBtn = document.getElementsByClassName("ex")[0];
+//   var flow = document.querySelector("body")
+//   var modal = document.querySelector("vid-modal")
+
+//   $(window).on('beforeunload', function () {
+//     $('body').hide();
+//     $(window).scrollTop(0);
+//   });
+
+
+//   exitBtn.onclick = function () {
+
+//     modal.style.display = "none";
+//     flow.style.overflowY = "scroll";
+
+//     console.log("working")
+
+//   }
